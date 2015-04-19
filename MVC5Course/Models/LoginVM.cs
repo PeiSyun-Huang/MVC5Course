2b66d0ac9c9023e0b3b8ec78901b437a0489d526
PeiSyun-Huang <pei.syun_0714@hotmail.com>
2015-04-19 11:30:38 +0800

    建立Login測試頁面，建立自訂M，GET/POST Action建置

---
+++ MVC5Course/Models/LoginVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string PassWord { get; set; }
+    }
+}
