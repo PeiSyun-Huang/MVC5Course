c58e0cdb6676d257bcd225133fe6b2e436f0275c
PeiSyun-Huang <pei.syun_0714@hotmail.com>
2015-04-25 15:50:41 +0800

    路由對應實作，可以惡搞網址

---
+++ MVC5Course/Controllers/JQueryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class JQueryController : Controller
+    {
+        // GET: JQuery
+        public ActionResult Index()
+        {
+            return RedirectToAction("each");
+        }
+
+        public ActionResult each()
+        {
+            return View();
+        }
+    }
+}
