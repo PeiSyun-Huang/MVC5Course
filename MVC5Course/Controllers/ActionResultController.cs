380d340df32a25f278ea43646c40d54e4bb150d8
PeiSyun-Huang <pei.syun_0714@hotmail.com>
2015-04-25 17:25:03 +0800

    ActionResult的ViewResult 與 ContentResult 實作

---
+++ MVC5Course/Controllers/ActionResultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ActionResultController : Controller
+    {
+        // GET: ActionResult
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View1()
+        {
+            return PartialView("View2");
+        }
+
+        public ActionResult View2()
+        {
+            return Content("<script>alert('OK!')</script>", "text/html");
+        }
+
+        public ActionResult SharedView()
+        {
+            return View("SharedView");
+        }
+    }
+}
