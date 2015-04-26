e934618b69380b94a2e68b10ced5a397f02e5b9d
PeiSyun-Huang <pei.syun_0714@hotmail.com>
2015-04-26 10:24:14 +0800

    練習 FileResult 的各種用法 ( 檔案路徑, 指定 Content-Type, byte[], 強制下載檔案 )

--- MVC5Course/Controllers/ActionResultController.cs
+++ MVC5Course/Controllers/ActionResultController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +28,49 @@ namespace MVC5Course.Controllers
         public ActionResult SharedView()
         {
             return View("SharedView");
+        }
+
+        // 本機 檔案路徑
+        public ActionResult File1()
+        {
+            return File(Server.MapPath("~/App_Data/logo11w.png"), "image/png");
+        }
+
+        // URL 檔案路徑
+        public ActionResult File2()
+        {
+            WebClient wc = new WebClient();
+            var data = wc.DownloadData("https://www.google.com.tw/images/srpr/logo11w.png");
+            return File(data, "image/png");
+        }
+
+        // URL 檔案路徑 強迫下載並存成指定檔案名稱
+        public ActionResult File3()
+        {
+            WebClient wc = new WebClient();
+            var data = wc.DownloadData("https://www.google.com.tw/images/srpr/logo11w.png");
+            return File(data, "image/png","Google.png");
+        }
+
+        // URL 檔案路徑 強迫下載並存成指定檔案名稱，需輸入參數 url / fileNamn
+        // 參數直接加在網址後面，url / fileName 有預設值
+        public ActionResult File4(string url = "https://www.google.com.tw/images/srpr/logo11w.png", string fileName = "Google.png")
+        {
+            WebClient wc = new WebClient();
+            var data = wc.DownloadData(url);
+            return File(data, "image/png", fileName);
+        }
+
+        // 輸出HTML
+        public ActionResult File5()
+        {
+            return File(Server.MapPath("~/Content/File5.html"),"text/html");
+        }
+
+        // 輸出Text
+        public ActionResult File6()
+        {
+            return File(Server.MapPath("~/Content/File5.html"), "text/plain");
         }
     }
 }
