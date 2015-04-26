5756476e3f3e40ebe9a9eaa12f548d475a48c218
PeiSyun-Huang <pei.syun_0714@hotmail.com>
2015-04-26 17:29:51 +0800

    練習 [HandleError] 自訂錯誤頁面的做法

---
+++ MVC5Course/Controllers/ActionFilterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class ActionFilterController : Controller
+    {
+        // GET: ActionFilter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HandleError(Master = "", ExceptionType = typeof(ArgumentException), View = "ErrorArgument")]
+        public ActionResult MadeMeWrong(string type = "")
+        {
+            if (type == "1")
+            {
+                throw new ArgumentException("ErrorArgument");
+            }
+            throw new Exception("Error");
+        }
+    }
+}
